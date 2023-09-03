99145d7f7ad786f2f0c645eb488c69c5a8e4b273
Cole <coleb229@yahoo.com>
2023-09-03 15:32:28 -0700

    fixing deployment

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="h-screen">
       <Navbar />
-      <Image src={JakerzIcon} width={400} height={400} id='jakerz' />
+      <Image src={JakerzIcon} width={400} height={400} id='jakerz' alt='Jakerz-icon' />
       <h1 className="text-9xl mx-auto text-white">Jakerz</h1>
     </main>
   )
