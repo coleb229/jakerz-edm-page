d1405e05728d5a7407d6e558d5b7568eae9dde78
Cole <coleb229@yahoo.com>
2023-09-03 18:41:42 -0700

    added some images to the gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,13 +1,61 @@
+"use client"
 import Image from 'next/image'
-import JakerzIcon from '../../public/jakerz-icon.png'
+import img1 from '@/public/images/img1.png'
 
 export default function Gallery() {
+
+  const expandImage = (e) => {
+    const img = e.target
+    img.classList.toggle('expanded')
+  }
+
   return (
     <main className="h-screen">
       <div id='content'>
         <h2>Gallery</h2>
-        <table>
-
+        <table id='gallery' className='m-5'>
+          <tr className=''>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+          </tr>
+          <tr className=''>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+          </tr>
+          <tr className=''>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+            <td onClick={expandImage}>
+              <Image src={img1} width={400} height={400} alt='img1' />
+            </td>
+          </tr>
         </table>
       </div>
     </main>
