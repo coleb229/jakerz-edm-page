ff51512c00f264bb0d6d0cd8251954fdc10aaa78
Cole <coleb229@yahoo.com>
2023-09-04 10:25:33 -0700

    Started working on the schedule page.  Using shadcn ui to make the table element.

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,12 +1,60 @@
 import Image from 'next/image'
 import JakerzIcon from '../../public/jakerz-icon.png'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function Schedule() {
   return (
     <main className="h-screen">
       <h2 id='header-route'>/Schedule</h2>
-      <Image src={JakerzIcon} width={400} height={400} id='jakerz' alt='Jakerz-icon' />
-      <h1 className="text-9xl mx-auto text-white">Schedule</h1>
+      <div id='content'>
+        <div className='w-1/2 mx-auto pt-10'>
+        <Table>
+          <TableCaption>A list of Jake's current scheduled shows.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[200px]">Club</TableHead>
+              <TableHead>Where</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Time</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium text-white">The LOFT at Skyway Theatre</TableCell>
+              <TableCell className='text-white'>Minneapolis, MN 55403</TableCell>
+              <TableCell className='text-white'>July 26, 2023</TableCell>
+              <TableCell className="text-right text-white">8pm - 2am</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-white">The LOFT at Skyway Theatre</TableCell>
+              <TableCell className='text-white'>Minneapolis, MN 55403</TableCell>
+              <TableCell className='text-white'>July 26, 2023</TableCell>
+              <TableCell className="text-right text-white">8pm - 2am</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-white">The LOFT at Skyway Theatre</TableCell>
+              <TableCell className='text-white'>Minneapolis, MN 55403</TableCell>
+              <TableCell className='text-white'>July 26, 2023</TableCell>
+              <TableCell className="text-right text-white">8pm - 2am</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium text-white">The LOFT at Skyway Theatre</TableCell>
+              <TableCell className='text-white'>Minneapolis, MN 55403</TableCell>
+              <TableCell className='text-white'>July 26, 2023</TableCell>
+              <TableCell className="text-right text-white">8pm - 2am</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        </div>
+      </div>
     </main>
   )
 }
