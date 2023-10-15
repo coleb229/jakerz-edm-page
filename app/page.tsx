49d37e601caa698c3818afde4f2fdb66b5f28753
Cole <coleb229@yahoo.com>
2023-10-14 23:33:40 -0700

    mmmmmm

--- app/page.tsx
+++ app/page.tsx
@@ -26,16 +26,13 @@ async function newUser() {
   const email = session?.user?.email
   const name = session?.user?.name
 
-  try {
-    await prisma.user.create({
-      data: {
-        email: email as string,
-        name: name as string,
-      },
-    })
-  } catch (error) {
-    console.error(error)
-  }
+  
+  await prisma.user.create({
+    data: {
+      email: email as string,
+      name: name as string,
+    },
+  })
 }
 
 export default async function Home() {
